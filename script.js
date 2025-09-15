ceeb9f268640929789e8433f2ab64f545f4b3058
AlanQ-Orbitron <166963943+AlanQ-Orbitron@users.noreply.github.com>
2025-09-15 03:15:41 +0000

    Commit New Change

--- script.js
+++ script.js
@@ -6,11 +6,30 @@ const windowHeight = window.innerHeight;
 const clickArrow = document.getElementById("arrow");
 const clone_clickArrow = clickArrow.cloneNode(true);
 document.body.appendChild(clone_clickArrow);
+document.body.appendChild(clickArrow); 
+
+class ElementGroup {
+    constructor(identity) {
+        this.identity = identity
+    }
+
+    forEach(callback) {
+        this.identity.forEach(callback);
+    }
+}
+
+const arrowGroup = new ElementGroup([clickArrow, clone_clickArrow]);
+arrowGroup.forEach(function(rs) {
+    rs.style.position = "absolute";
+})
+
 const maxwell = document.getElementById("maxwell_div");
+
+
 
 //audio
 const maxwell_loop = document.getElementById("maxwell_loop")
-//maxwell_loop.play()
+maxwell_loop.play()
 
 var tick = 0, click = 0;
 var velocity;
@@ -19,10 +38,9 @@ var cursor_pos_x, cursor_pos_y;
 var isJumping;
 var isMoving;
 var isAlreadyRan = true;
-var isAvoidingCursor = true;
+var isAvoidingCursor = false;
 
 var increamentX = 1, increamentY = 1;
-
 
 //document events
 document.addEventListener("mousemove", mouseMove)
@@ -34,16 +52,18 @@ const rtTimer = setInterval(function() {
 
     if (isAlreadyRan) {
         clickArrow.refresh();
-        clone_clickArrow.positionSet(clickArrow.rect.left, clickArrow.rect.top);
         clone_clickArrow.refresh();
+        clone_clickArrow.positionSet(clickArrow.rect.left, clickArrow.rect.top);
         isAlreadyRan = false;
     }
 
-    clickArrow.slideAndMove(0, 0.98);
-    clone_clickArrow.slideAndMove(0, 0.98);
+    arrowGroup.forEach(function(rs) {
+        rs.slideAndMove(0, 0.98)
+    })
 
     if (isAvoidingCursor) {
-        avoidCursor();
+        avoidCursor(clickArrow, arrowGroup);
+        avoidCursor(clone_clickArrow, arrowGroup);
     }
 
     tick = (tick >= 59) ? 0 : tick + 1;
@@ -63,55 +83,54 @@ function mouseMove(event){
 
 function mb1ClickEventRun(){
     click++;
-    if (click < 5) {
+    if (click < 2) {
         isJumping = true;
         jump();
+    } else if (click < 5) {
+        jump();
+        isAvoidingCursor = false
     } else if (click < 20) {
         isJumping = false;
     }
 }
 
-function avoidCursor(){
-    clickArrow.refresh();
-    var cursorDistance = ((clickArrow.originX - cursor_pos_x) ** 2 + (clickArrow.originY - cursor_pos_y) ** 2) ** 0.5;
-    if (cursorDistance <= (((windowWidth ** 2 + windowHeight ** 2) ** 0.5) * 0.05)) {
-        clickArrow.angle = Math.atan2((clickArrow.originY - cursor_pos_y), (clickArrow.originX - cursor_pos_x));
-        clone_clickArrow.angle = clickArrow.angle;
-        clickArrow.slideAndMove(5, 0.98)
-        clone_clickArrow.slideAndMove(5, 0.98)
-    } else if (cursorDistance <= (((windowWidth ** 2 + windowHeight ** 2) ** 0.5) * 0.1)) {
-        clickArrow.angle = Math.atan2((clickArrow.originY - cursor_pos_y), (clickArrow.originX - cursor_pos_x));
-        clone_clickArrow.angle = clickArrow.angle;
-        clickArrow.slideAndMove(2, 0.98)
-        clone_clickArrow.slideAndMove(2, 0.98)
+function avoidCursor(DOM, Group){
+    DOM.refresh();
+    var cursorDistance = ((DOM.originX - cursor_pos_x) ** 2 + (DOM.originY - cursor_pos_y) ** 2) ** 0.5;
+    if (cursorDistance <= (((windowWidth ** 2 + windowHeight ** 2) ** 0.5) * 0.01)) {
+        DOM.angle = Math.atan2((DOM.originY - cursor_pos_y), (DOM.originX - cursor_pos_x));
+        Group.forEach(function(rs) {
+            rs.angle = DOM.angle;
+            rs.slideAndMove(4, 0.98);
+            rs.wrapAround();
+        })
+    } else if (cursorDistance <= (((windowWidth ** 2 + windowHeight ** 2) ** 0.5) * 0.05)) {
+        DOM.angle = Math.atan2((DOM.originY - cursor_pos_y), (DOM.originX - cursor_pos_x));
+        Group.forEach(function(rs) {
+            rs.angle = DOM.angle;
+            rs.slideAndMove(1, 0.98);
+            rs.wrapAround();
+        })
     }
-    var cursorDistance = ((clone_clickArrow.originX - cursor_pos_x) ** 2 + (clone_clickArrow.originY - cursor_pos_y) ** 2) ** 0.5;
-    if (cursorDistance <= (((windowWidth ** 2 + windowHeight ** 2) ** 0.5) * 0.05)) {
-        clone_clickArrow.angle = Math.atan2((clone_clickArrow.originY - cursor_pos_y), (clone_clickArrow.originX - cursor_pos_x));
-        clickArrow.angle = clone_clickArrow.angle;
-        clickArrow.slideAndMove(5, 0.98)
-        clone_clickArrow.slideAndMove(5, 0.98)
-    } else if (cursorDistance <= (((windowWidth ** 2 + windowHeight ** 2) ** 0.5) * 0.1)) {
-        clone_clickArrow.angle = Math.atan2((clone_clickArrow.originY - cursor_pos_y), (clone_clickArrow.originX - cursor_pos_x));
-        clickArrow.angle = clone_clickArrow.angle;
-        clickArrow.slideAndMove(2, 0.98)
-        clone_clickArrow.slideAndMove(2, 0.98)
-    }
-
 }
 
 function DVD(DOM) {
     DOM.refresh();
 
-    if (DOM.rect.left <= 0 || DOM.rect.left + DOM.rect.width >= window.innerWidth) {increamentX *= -1;}
-    if (DOM.rect.top <= 0 || DOM.rect.top + DOM.rect.height >= window.innerHeight) {increamentY *= -1;}
+    if (DOM.isScreenBorderTouch("horizontal")) {increamentX *= -1;}
+    if (DOM.isScreenBorderTouch("vertical")) {increamentY *= -1;}
 
     DOM.positionAdd(increamentX, increamentY);
 }
 
 //Jumping Sequence
 function jump() {
-    clickArrow.positionSet(Math.floor(Math.random() * (windowWidth - clickArrow.rect.width)), Math.floor(Math.random() * (windowHeight - clickArrow.rect.height)));
+    var x = Math.floor(Math.random() * (windowWidth - clickArrow.rect.width))
+    var y = Math.floor(Math.random() * (windowHeight - clickArrow.rect.height))
+    arrowGroup.forEach(function(rs) {
+        rs.positionSet(x , y);
+    })
+    
 }
 
 // #region helper functions
@@ -153,6 +172,41 @@ HTMLElement.prototype.slideAndMove = function(velocity, friction) {
         isMoving = false
     }  else if (!this.isMoving) {
         this.isMoving = true
+    }
+}
+
+HTMLElement.prototype.isScreenBorderTouch = function(type) {
+    switch(type) {
+        case undefined:
+            return this.rect.left < 0 || this.rect.right > window.innerWidth || this.rect.top < 0 || this.rect.bottom > window.innerHeight;
+        case "horizontal":
+            return this.rect.left < 0 || this.rect.right > window.innerWidth;
+        case "left":
+            return this.rect.left < 0;
+        case "right":
+            return this.rect.right > window.innerWidth;
+        case "vertical":
+            return this.rect.top < 0 || this.rect.bottom > window.innerHeight;
+        case "top":
+            return this.rect.top < 0;
+        case "bottom":
+            return this.rect.bottom > window.innerHeight;
+    }
+}
+
+HTMLElement.prototype.wrapAround = function() {
+    this.refresh();
+    if (this.isScreenBorderTouch("right")) {
+        this.style.left = this.rect.left - window.innerWidth - this.rect.width + "px";
+    }
+    if (this.isScreenBorderTouch("left")) {
+        this.style.left = this.rect.left + window.innerWidth + this.rect.width + "px";
+    }
+    if (this.isScreenBorderTouch("top")) {
+        this.style.top = this.rect.top - window.innerHeight - this.rect.height + "px";
+    }
+    if (this.isScreenBorderTouch("bottom")) {
+        this.style.top = this.rect.top + window.innerHeight + this.rect.height + "px";
     }
 }
 
